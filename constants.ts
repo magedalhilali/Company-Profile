4e6815571c37453cf5522460998342159919850c
magedalhilali <maged.alhilali705@gmail.com>
2025-12-26 16:34:08 +0400

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,17 +1,33 @@
-// Generating 26 high-quality placeholder images acting as portfolio pieces.
-// We use picsum with specific seeds to ensure consistency and caching.
-
 const IMAGE_COUNT = 26;
 
-export const PORTFOLIO_IMAGES = Array.from({ length: IMAGE_COUNT }).map((_, i) => {
-  // Using different seeds to get different images
-  const seed = i + 100; 
-  // Alternating orientations slightly to test object-fit logic, 
-  // though mostly we want portrait for a book format.
-  return `https://picsum.photos/seed/${seed}/800/1200`;
-});
+export const PORTFOLIO_IMAGES = [
+  "https://static.wixstatic.com/media/756a6a_d44678160de04d6e9873ef243e9a6525~mv2.png",   // Page 1 (Cover)
+  "https://static.wixstatic.com/media/756a6a_205b955cd14c4bc1a933df2ba7e68243~mv2.png",      // Page 2
+  "https://static.wixstatic.com/media/756a6a_fe0df94167014ab1b3f3e2ec818fb640~mv2.png",      // Page 3
+  "https://static.wixstatic.com/media/756a6a_4507d4ec5c044d09b4723cf9a6616c11~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_7fe275aebbaf4e3794c71e56e156d1af~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_de5e083125724ae4b3d14399436bbf51~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_077584f17a1f48c5b2d8c9561ba82cc3~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_d62835b8d2b04c2d9b51f830fa1fcf26~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_b8174796fd5d4bd98ef11d0c39475b4e~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_a1366bab3b654881930dd700039e89ad~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_920cf2e7c2db48e39cf4ee9fe585d9e2~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_a39b501e101542c19e6ec62ee2304d8b~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_396691ed21ec497299dd378177ba4885~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_99ec212091194895a798fc789b48d33a~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_5c039fe188b543c99a3cce57c839fedd~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_2dc0376121184560bde16408883151f3~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_cd0ad7d539d94575b50dde2ac6a64433~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_da22730a067b46b4b4767396e9fd0a4c~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_a6240f4c044644f0a04ab2e117c43760~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_e9a3acaacc5643259384fb3c80fb8ced~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_d820b851a3d548ce8d087e75114cbd0a~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_3521069c813a48a4a5d662b7b6c41ddf~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_af83dca278f24fe78fcde022d04df9e2~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_8e605bbadfa4443d9efc60ec253d7ab3~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_11a042bd30e74225b77ace513cf2aa8a~mv2.png", 
+  "https://static.wixstatic.com/media/756a6a_92a8a5e8b16c4ddd91c5e74cd856abb4~mv2.png"   // Page 26 (Back Cover)
+];
 
 export const TOTAL_PAGES = PORTFOLIO_IMAGES.length;
-// A book has sheets. Each sheet has 2 pages (Front/Back).
-// 26 pages = 13 sheets.
 export const TOTAL_SHEETS = Math.ceil(TOTAL_PAGES / 2);
